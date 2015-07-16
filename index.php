3b3f95913cacc55af4229b1394fbc08d47895077
lan <lan@swcombine.com>
2015-07-15 22:24:43 -0400

    Fixed error

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 	$maintenance = true;
 	if ($maintenance) {
-		echo '<h1 style="text-align:center">Down for Maintenance</h1>";
+		echo '<h1 style="text-align:center">Down for Maintenance</h1>';
 	}
